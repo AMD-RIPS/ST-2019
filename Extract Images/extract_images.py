f9f9ac64db25982238c67aa2ed8fa73e09a126dd
Kevin Jiang <kuanhaoj@seas.upenn.edu>
2019-07-19 17:52:39 -0700

    Updated image extraction

--- Extract Images/extract_images.py
+++ Extract Images/extract_images.py
@@ -8,27 +8,109 @@ output_type = sys.argv[3]
 interval = int(sys.argv[4])
 # max_num = int(sys.argv[5])
 
-cap = cv2.VideoCapture(input_video_path)
 
 count = 0
 num_produced = 0
 
+
+def is_video_file(filename):
+	video_file_extensions = (
+	'.264', '.3g2', '.3gp', '.3gp2', '.3gpp', '.3gpp2', '.3mm', '.3p2', '.60d', '.787', '.89', '.aaf', '.aec', '.aep', '.aepx',
+	'.aet', '.aetx', '.ajp', '.ale', '.am', '.amc', '.amv', '.amx', '.anim', '.aqt', '.arcut', '.arf', '.asf', '.asx', '.avb',
+	'.avc', '.avd', '.avi', '.avp', '.avs', '.avs', '.avv', '.axm', '.bdm', '.bdmv', '.bdt2', '.bdt3', '.bik', '.bin', '.bix',
+	'.bmk', '.bnp', '.box', '.bs4', '.bsf', '.bvr', '.byu', '.camproj', '.camrec', '.camv', '.ced', '.cel', '.cine', '.cip',
+	'.clpi', '.cmmp', '.cmmtpl', '.cmproj', '.cmrec', '.cpi', '.cst', '.cvc', '.cx3', '.d2v', '.d3v', '.dat', '.dav', '.dce',
+	'.dck', '.dcr', '.dcr', '.ddat', '.dif', '.dir', '.divx', '.dlx', '.dmb', '.dmsd', '.dmsd3d', '.dmsm', '.dmsm3d', '.dmss',
+	'.dmx', '.dnc', '.dpa', '.dpg', '.dream', '.dsy', '.dv', '.dv-avi', '.dv4', '.dvdmedia', '.dvr', '.dvr-ms', '.dvx', '.dxr',
+	'.dzm', '.dzp', '.dzt', '.edl', '.evo', '.eye', '.ezt', '.f4p', '.f4v', '.fbr', '.fbr', '.fbz', '.fcp', '.fcproject',
+	'.ffd', '.flc', '.flh', '.fli', '.flv', '.flx', '.gfp', '.gl', '.gom', '.grasp', '.gts', '.gvi', '.gvp', '.h264', '.hdmov',
+	'.hkm', '.ifo', '.imovieproj', '.imovieproject', '.ircp', '.irf', '.ism', '.ismc', '.ismv', '.iva', '.ivf', '.ivr', '.ivs',
+	'.izz', '.izzy', '.jss', '.jts', '.jtv', '.k3g', '.kmv', '.ktn', '.lrec', '.lsf', '.lsx', '.m15', '.m1pg', '.m1v', '.m21',
+	'.m21', '.m2a', '.m2p', '.m2t', '.m2ts', '.m2v', '.m4e', '.m4u', '.m4v', '.m75', '.mani', '.meta', '.mgv', '.mj2', '.mjp',
+	'.mjpg', '.mk3d', '.mkv', '.mmv', '.mnv', '.mob', '.mod', '.modd', '.moff', '.moi', '.moov', '.mov', '.movie', '.mp21',
+	'.mp21', '.mp2v', '.mp4', '.mp4v', '.mpe', '.mpeg', '.mpeg1', '.mpeg4', '.mpf', '.mpg', '.mpg2', '.mpgindex', '.mpl',
+	'.mpl', '.mpls', '.mpsub', '.mpv', '.mpv2', '.mqv', '.msdvd', '.mse', '.msh', '.mswmm', '.mts', '.mtv', '.mvb', '.mvc',
+	'.mvd', '.mve', '.mvex', '.mvp', '.mvp', '.mvy', '.mxf', '.mxv', '.mys', '.ncor', '.nsv', '.nut', '.nuv', '.nvc', '.ogm',
+	'.ogv', '.ogx', '.osp', '.otrkey', '.pac', '.par', '.pds', '.pgi', '.photoshow', '.piv', '.pjs', '.playlist', '.plproj',
+	'.pmf', '.pmv', '.pns', '.ppj', '.prel', '.pro', '.prproj', '.prtl', '.psb', '.psh', '.pssd', '.pva', '.pvr', '.pxv',
+	'.qt', '.qtch', '.qtindex', '.qtl', '.qtm', '.qtz', '.r3d', '.rcd', '.rcproject', '.rdb', '.rec', '.rm', '.rmd', '.rmd',
+	'.rmp', '.rms', '.rmv', '.rmvb', '.roq', '.rp', '.rsx', '.rts', '.rts', '.rum', '.rv', '.rvid', '.rvl', '.sbk', '.sbt',
+	'.scc', '.scm', '.scm', '.scn', '.screenflow', '.sec', '.sedprj', '.seq', '.sfd', '.sfvidcap', '.siv', '.smi', '.smi',
+	'.smil', '.smk', '.sml', '.smv', '.spl', '.sqz', '.srt', '.ssf', '.ssm', '.stl', '.str', '.stx', '.svi', '.swf', '.swi',
+	'.swt', '.tda3mt', '.tdx', '.thp', '.tivo', '.tix', '.tod', '.tp', '.tp0', '.tpd', '.tpr', '.trp', '.ts', '.tsp', '.ttxt',
+	'.tvs', '.usf', '.usm', '.vc1', '.vcpf', '.vcr', '.vcv', '.vdo', '.vdr', '.vdx', '.veg','.vem', '.vep', '.vf', '.vft',
+	'.vfw', '.vfz', '.vgz', '.vid', '.video', '.viewlet', '.viv', '.vivo', '.vlab', '.vob', '.vp3', '.vp6', '.vp7', '.vpj',
+	'.vro', '.vs4', '.vse', '.vsp', '.w32', '.wcp', '.webm', '.wlmp', '.wm', '.wmd', '.wmmp', '.wmv', '.wmx', '.wot', '.wp3',
+	'.wpl', '.wtv', '.wve', '.wvx', '.xej', '.xel', '.xesc', '.xfl', '.xlmv', '.xmv', '.xvid', '.y4m', '.yog', '.yuv', '.zeg',
+	'.zm1', '.zm2', '.zm3', '.zmv'  )
+
+	if filename.endswith((video_file_extensions)):
+		return True
+	else:
+		return False
+
 if not os.path.isdir(output_image_path):
 	os.mkdir(output_image_path)
 
-while(1):
-    ret,frame = cap.read()
+is_input_folder = False
 
-    if not ret:
-        break
+if os.path.isdir(input_video_path):
+	is_input_folder = True
 
-    if count % interval == 0:
-        output_name = os.path.join(output_image_path, str(count) + '.' + output_type)
-        cv2.imwrite(output_name, frame)
-        num_produced += 1
+if not is_input_folder:
+	cap = cv2.VideoCapture(input_video_path)
+	while(1):
+		ret,frame = cap.read()
 
-    count += 1
-cap.release()
-print("Number of images extracted: " + str(num_produced))
+		if not ret:
+			break
 
-print("Rename the output folder or choose a different output folder before running the program again (to prevent overwriting exsiting images)")
+		if count % interval == 10:
+			output_name = os.path.join(output_image_path, str(num_produced) + '.' + output_type)
+			cv2.imwrite(output_name, frame)
+			num_produced += 1
+			if num_produced % 50 == 0:
+				print(str(num_produced) + " images extracted")
+
+		count += 1
+	cap.release()
+	print("Number of images extracted from the video: " + str(num_produced))
+
+	print("Rename the output folder or choose a different output folder before running the program again (to prevent overwriting exsiting images)")
+
+else:
+	total_num_images = 0
+	file_count = 0
+	count = 0
+	for video_path in os.listdir(input_video_path):
+		print(video_path)
+		if not is_video_file(video_path):
+			continue
+
+		cap = cv2.VideoCapture(os.path.join(input_video_path, video_path))
+
+		while(1):
+			ret,frame = cap.read()
+
+			if not ret:
+				break
+
+			if count % interval == 10:
+				output_name = os.path.join(output_image_path, str(file_count) +  '_' + str(num_produced) + '.' + output_type)
+				cv2.imwrite(output_name, frame)
+				num_produced += 1
+
+				if num_produced % 50 == 0:
+					print(str(num_produced) + " images extracted")
+
+			count += 1
+		cap.release()
+		print("Number of images extracted from the video: " + str(num_produced))
+		total_num_images += num_produced
+		file_count += 1
+
+	print("Finished extracting " + str(total_num_images) + " images from "+ str(file_count) + " videos." )
+	print("Rename the output folder or choose a different output folder before running the program again (to prevent overwriting exsiting images)")
+
+
+
