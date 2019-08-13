b2a4d75741994b7bdce2122b3217d530457a2b9a
Kevin Jiang <kuanhaoj@seas.upenn.edu>
2019-08-13 00:05:57 -0700

    reduce runtime of random patch by factor of 20

--- Individual Glitch Classifier/random_patch_test/random_patch_test.py
+++ Individual Glitch Classifier/random_patch_test/random_patch_test.py
@@ -6,12 +6,14 @@ import sys
 import timeit
 from scipy import stats
 from sklearn.metrics import confusion_matrix
+import time
 
 
 # Compute the abnormality score for each pixel in the input img
 def compute_salience_matrix(img):
-	width, height, channel = img.shape
-	num_pixels = height * width
+	width = h
+	height = w
+	num_pixels = area
 	feature_size = 3
 
 	mu = np.average(np.average(img, axis = 0), axis = 0)
@@ -32,94 +34,86 @@ def compute_salience_matrix(img):
 	L_sym = LA.multi_dot([D_sqrt_inv, L, D_sqrt_inv])
 
 	salience_matrix = np.empty([width, height])
-	for i in range(width):
-		for j in range(height):
-			mean_centered_pixel = img[i,j,:] - mu
-			salience_matrix[i, j] = LA.multi_dot([mean_centered_pixel, L, mean_centered_pixel])
+
+	# st = time.time()
+
+	# print(mu[None, None, :])
+
+
+
+
+	img = img.astype(float)
+
+
+	# for i in range(width):
+	# 	for j in range(height):
+	# 		mean_centered_pixel = img[i,j,:] - mu
+
+	# 		if i == 10 and j == 10:
+	# 			print(mean_centered_pixel)
+	# 		salience_matrix[i, j] = LA.multi_dot([mean_centered_pixel, L, mean_centered_pixel])
+	# print(salience_matrix[:5,:5])
+
+
+	img[:,:,0] = img[:,:,0] - mu[0]
+	img[:,:,1] = img[:,:,1] - mu[1]
+	img[:,:,2] = img[:,:,2] - mu[2]
+
+
+	z2 = np.einsum("ijk, lk -> ijl", img, L)
+	salience_matrix = np.einsum("ijk, ijk -> ij", img, z2)
+
+
 
 	return salience_matrix
 
 
 def is_glitched(img):
-    h,w,_ = img.shape
+	h,w,_ = img.shape
 
-    # M = 50
-    # N = 50
+	M = int(h / 21)
+	N = int(w / 38)
 
-    M = int(h / 21)
-    N = int(w / 38)
+	count = 0
 
+	for i in range(0,h-M,M):
+		for j in range(0, w-N, N):
+			tile = np.mean(img[i:i+M, j:j+N, :], axis = 2)
 
-    result = np.zeros_like(img)
-    count = 0
-
-    for i in range(0,h-M,M):
-        for j in range(0, w-N, N):
-            tile = np.mean(img[i:i+M, j:j+N, :], axis = 2)
-
-
-            if np.all(tile == tile[0,0]) and tile[0,0] > 40:
-                result[i:i+M, j:j+N,:]=255
-                count += 1
-
-    if count > 20:
-    	return 1
-    else:
-    	return 0
-
-# def is_glitched(img):
-#     h,w,_ = img.shape
-
-#     M = 30
-#     N = 30
+			if np.all(tile == tile[0,0]) and tile[0,0] > 40:
+				count += 1
+				if count > 20:
+					return 1
 
 
-#     result = np.zeros_like(img)
-#     count = 0
-
-#     for i in range(0,h-M,M):
-#         for j in range(0, w-N, N):
-#             tile = np.mean(img[i:i+M, j:j+N, :], axis = 2)
+	return 0
 
 
-#             if np.all(tile == tile[0,0]) and tile[0,0] > 40:
-#                 result[i:i+M, j:j+N,:]=255
-#                 count += 1
-
-#     if count > 50:
-#     	return 1, result, tile[0,0]
-#     else:
-#     	return 0, result, tile[0,0]
-
+h, w = 270, 480
+area = h * w
 
 
 def classify(img):
-	img = cv2.resize(img, (480, 270))
-	# test_img = np.zeros_like(img)
-	h,w,_ = img.shape
-	area = h * w
-
+	img = cv2.resize(img, (w, h))
 
 
 	salience_matrix = compute_salience_matrix(img)
+
 	salience_list = np.reshape(salience_matrix, [area])
 	var = np.var(salience_list)
 	std = np.sqrt(var)
 	salience_threshold = np.mean(salience_list) + 2 * std
 
-	for i in range(h):
-		for j in range(w):
-			if salience_matrix[i,j] <= salience_threshold:
-				img[i,j,:] = 0
-			# else:
-			# 	test_img[i,j,:] = 255
+	# print(salience_threshold)
 
 
-	# plt.imshow(img)
-	# plt.show()
+	img[np.where(salience_matrix <= salience_threshold)] = 0
 
-	# plt.imshow(test_img)
-	# plt.show()
+	# for i in range(h):
+	# 	for j in range(w):
+	# 		if salience_matrix[i,j] <= salience_threshold:
+	# 			img[i,j,:] = 0
+
 
 	
 	flat = img.flatten()
@@ -138,7 +132,22 @@ def test(X):
 	y = np.empty(n)
 
 	for i in range(n):
-		y[i] = classify(X[i,:,:,:]) | is_glitched(X[i,:,:,:])
+
+		# st = time.time()
+
+		if is_glitched(X[i, :, :, :]):
+			y[i] = 1
+		elif classify(X[i, :, :, :]):
+			y[i] = 1
+		else:
+			y[i] = 0
+
+		# ed = time.time()
+		# print(ed - st)
+
+
+
+		# y[i] = a | b
 
 	return y
 
@@ -151,8 +160,8 @@ def test(X):
 # X_train_1 = np.load("/home/IPAMNET/kjiang/Desktop/glitch_classifiers/normal_data/X_test_normal.npy")
 # X_train_3 = np.load(os.path.join(wd, "X_test_" + glitch_type + ".npy"))
 
-# X_train_1 = X_train_1[:40,:,:,:]
-# X_train_3 = X_train_3[:40,:,:,:]
+# # X_train_1 = X_train_1[:40,:,:,:]
+# # X_train_3 = X_train_3[:40,:,:,:]
 
 # y_1 = np.zeros(X_train_1.shape[0])
 # y_3 = np.ones(X_train_3.shape[0])
@@ -168,7 +177,13 @@ def test(X):
 # idx = np.random.permutation(X_train.shape[0])
 # X_train, y_train = X_train[idx], y_train[idx]
 
+
+
+# st = time.time()
 # y_pred = test(X_train)
+# ed = time.time()
+
+# print("total", ed - st)
 
 
 
@@ -187,50 +202,5 @@ def test(X):
 
 
 
-
-
-
-
-
-
-# h, w = 270, 480
-# area = h * w
-
-
-
-# correct = 0.0
-# total = 0.0
-
-# for i in range(X_train.shape[0]):
-# 	img = X_train[i,:,:,:]
-# 	p, t, l = classify(img)
-# 	q, r, color = is_glitched(img)
-
-# 	y_pred[i] = ((p + q) > 0) * 1
-# 	# print(y_train[i], p, q)
-
-
-# 	# if y_train[i] != y_pred[i]:
-# 	# 	print(color, l)
-# 	# 	plt.imshow(img)
-# 	# 	plt.show()
-
-# 	# 	plt.imshow(r)
-# 	# 	plt.show()
-
-# 	# 	plt.imshow(t)
-# 	# 	plt.show()
-# 	total += 1
-# 	if y_train[i] == y_pred[i]:
-# 		correct += 1
-
-# 	if i % 20 == 0:
-# 		print("accuracy", total, correct / total)
-
-
-
-# matrix = confusion_matrix(y_train, y_pred)
-
-# print(matrix)
 
 
