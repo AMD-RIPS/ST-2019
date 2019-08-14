89d7499d7c92885b3af7ce24bf614cf708101554
Kevin Jiang <kuanhaoj@seas.upenn.edu>
2019-08-13 19:19:02 -0700

    improved line pixelation

--- Individual Glitch Classifier/line_pixelation_test/line_pixelation_test.py
+++ Individual Glitch Classifier/line_pixelation_test/line_pixelation_test.py
@@ -7,6 +7,8 @@ from sklearn.metrics import confusion_matrix
 from scipy import ndimage
 from sklearn.linear_model import LogisticRegression
 from numpy import linalg as LA
+import time
+from functools import partial
 
 
 # h, w = 1080, 1920
@@ -16,7 +18,7 @@ from numpy import linalg as LA
 
 
 def compute_salience_matrix(img):
-	width, height, _ = img.shape
+	width, height,_ = img.shape
 	# num_pixels = width * height
 	feature_size = 3
 
@@ -51,11 +53,24 @@ def compute_salience_matrix(img):
 
 
 
+def add_neighbors(a, salience_threshold, interval):
+	z = np.zeros_like(a)
+
+	length = a.shape[0]
+	for i in range(length):
+		if a[i] > salience_threshold:
+			z[i-interval:i+interval] = 1
+
+	return np.count_nonzero(z) / float(length)
+
+
+
 def classify(img):
 	h,w,_ = img.shape
 	h1 = float(h)
 	w1 = float(w)
 	p = np.zeros([h,w])
+	
 
 	salience_matrix = compute_salience_matrix(img)
 
@@ -64,18 +79,35 @@ def classify(img):
 	std = np.sqrt(var)
 	salience_threshold = max(np.mean(salience_list) + 2 * std, 6000)
 
-	interval = 10
+	interval = h // 108
+
+	# ed = time.time()
+
+	# st = time.time()
+	# aa = np.amax(np.apply_along_axis(add_neighbors, 0, salience_matrix, salience_threshold = salience_threshold, interval = interval))
+	# bb = np.amax(np.apply_along_axis(add_neighbors, 1, salience_matrix, salience_threshold = salience_threshold, interval = interval))
+	salience_matrix = salience_matrix.tolist()
+
 	for i in range(h):
 		for j in range(w):
-			if salience_matrix[i, j] > salience_threshold:
+			if salience_matrix[i][j] > salience_threshold:
 				p[i - interval :i+ interval,j] = 1
 				p[i,j-interval:j+interval] = 1
 
-	p[salience_matrix > salience_threshold] = 1
+	salience_matrix = np.array(salience_matrix)
+
+	# ed = time.time()
+
+	# p[salience_matrix > salience_threshold] = 1
+	# print(ed - st)
 
 
 	a = np.amax(np.count_nonzero(p, axis = 0) / h1)
 	b = np.amax(np.count_nonzero(p, axis = 1) / w1)
+
+	# print(a,b,aa, bb)
+
+	# print(ed - st)
 
 	return max(a,b)
 
@@ -110,7 +142,7 @@ def test(X):
 
 	for i in range(n):
 		# print(classify(X[i,:,:,:]))
-		rt.append(classify(X[i,:,:,:]) > 0.6)
+		rt.append(classify(X[i,:,:,:]) > 0.65)
 
 	return np.asarray(rt)
 
@@ -124,31 +156,26 @@ def test(X):
 
 
 def main():
-	# glitch_type = "line_pixelation"
-	# wd = "np_data"
+	glitch_type = "line_pixelation"
+	wd = "np_data"
 
-	# X_test_1 = np.load("/home/IPAMNET/kjiang/Desktop/glitch_classifiers/normal_data/X_test_normal.npy")
-	# #X_test_1 = transform(X_test_1,)
-
-
-	# X_test_2 = np.load(os.path.join(wd, "X_test_" + glitch_type + ".npy"))
-	# #X_test_2 = transform(X_test_2, 1)
-
-
-	# test(X_test_2)
+	X_test_1 = np.load("/home/IPAMNET/kjiang/Desktop/glitch_classifiers/normal_data/X_test_normal.npy")
+	X_test_2 = np.load(os.path.join(wd, "X_test_" + glitch_type + ".npy"))
 
 
 
-	# y_1 = np.zeros(X_test_1.shape[0])
-	# y_2 = np.ones(X_test_2.shape[0])
+	y_1 = np.zeros(X_test_1.shape[0])
+	y_2 = np.ones(X_test_2.shape[0])
 
-	# X_test = np.concatenate((X_test_1, X_test_2))
-	# y_test = np.concatenate((y_1, y_2))
+	X_test = np.concatenate((X_test_1, X_test_2))
+	y_test = np.concatenate((y_1, y_2))
 
+	st = time.time()
+	y_pred = test(X_test)
+	ed = time.time()
+	print(ed - st)
 
-	# y_pred = test(X_test)
-
-	# print(confusion_matrix(y_test, y_pred))
+	print(confusion_matrix(y_test, y_pred))
 
 
 
